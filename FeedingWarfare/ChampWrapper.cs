86729220a568e03dc6b7ce2fc9f0b44780158c66
WarfareLoL <rmalphaa@gmail.com>
2016-08-25 22:44:04 -0400

    Add files via upload

---
+++ FeedingWarfare/ChampWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using LeagueSharp;
+
+namespace FeedingWarfare
+{
+    public class ChampWrapper
+    {
+        public string Name { get; set; }
+
+        public List<SpellSlot> SpellSlots { get; set; }
+    }
+}
